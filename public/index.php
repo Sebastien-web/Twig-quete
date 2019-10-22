17d4fb527050a1d756a26e47b61824e425f2c8df
Sebastien Comberousse <scomberousse@gmail.com>
2019-10-22 15:46:08 +0200

    Projet fini

--- public/index.php
+++ public/index.php
@@ -1,9 +1,8 @@
 <?php
 
-require 'vendor/autoload.php';
-$loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../View');
+require '../vendor/autoload.php';
+$loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/View');
 $twig = new Twig\Environment($loader);
 
 $products = ['product1', 'product2', 'product3', 'product4', 'product5'];
-echo $twig->render('index.html.twig',$products);
-
+echo $twig->render('index.html.twig',['products'=>$products]);
